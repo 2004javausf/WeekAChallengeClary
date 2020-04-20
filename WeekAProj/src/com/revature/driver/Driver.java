b0771703209b909889466ed68b26979d12fdbe24
Jason W. Clary <jwclary411@gmail.com>
2020-04-20 17:10:59 -0400

    Added functionality to the menu system

--- WeekAProj/src/com/revature/driver/Driver.java
+++ WeekAProj/src/com/revature/driver/Driver.java
@@ -1,5 +1,10 @@
 package com.revature.driver;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
+import com.revature.beans.Animal;
+import com.revature.beans.Human;
 import com.revature.beans.Validate;
 
 public class Driver {
@@ -7,9 +12,13 @@ public class Driver {
 // Scanner to take in user input
 	public static Scanner userInput = new Scanner(System.in);
 	
+// Lists to store objects
+	static List<Human> humans = new ArrayList<Human>();
+	static List<Animal> animals = new ArrayList<Animal>();
+	static List<Integer> ages = new ArrayList<Integer>();
+	
 	public static void main(String[] args) {
 		MainMenu();
-
 	}
 	
 // Menu Switch case
@@ -19,6 +28,7 @@ public class Driver {
 		System.out.println("----------------------------------");
 		System.out.println("1) Human");
 		System.out.println("2) Animal");
+		System.out.println("3) Average Human's Age");
 		
 		// Store input in usable variable
 		String reply = userInput.nextLine();
@@ -34,9 +44,65 @@ public class Driver {
 		MainMenu();
 		break;
 	case 1:
+		// Create a new person to add features to
+		Human person = new Human();
+		
+		// Get name
+		System.out.println("What is your humans name?");
+		person.setName(userInput.nextLine());	
+		
+		// Get age
+		System.out.println("\nWhat is " + person.getName() + "'s age?");
+		String age = userInput.nextLine();
+		if (Validate.CheckInt(age,"Please enter your response in whole number format.")) {
+			person.setAge(Integer.parseInt(age));
+		}else {person.setAge(0);}
+		ages.add(person.getAge());
+		
+		// Get gender
+		System.out.println("\nWhat is " + person.getName() + "'s gender?");
+		person.setGender(userInput.nextLine());
+		
+		// Add new person
+		humans.add(person);
+		
+		// Loop back to the menu
+		System.out.println("\n\n\n\n\n");
+		MainMenu();
 		break;
+		
 	case 2:
-		break;	
+		// Create a new animal to add features to
+		Animal pet = new Animal();
+		
+		// Get name
+		System.out.println("What is the animals name?");
+		pet.setName(userInput.nextLine());
+		
+		// Get species
+		System.out.println("What is " + pet.getName() + "'s species?");
+		pet.setSpecies(userInput.nextLine());
+		
+		// Get color
+		System.out.println("What is " + pet.getName() + "'s color?");
+		pet.setColor(userInput.nextLine());
+		
+		// Add new animal
+		animals.add(pet);
+		
+		// Loop back to the menu
+	    System.out.println("\n\n\n\n\n");
+		MainMenu();
+		break;
+		
+	case 3:
+		// Loop through the ages List and get the average
+		int average = 0;
+		for (int i : ages) {
+			average += i;
+		}
+		System.out.println("\n" + average / ages.size());
+		break;
 	}
 }
 	
